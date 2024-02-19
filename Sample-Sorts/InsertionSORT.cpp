5874d97c83359dc02e98c15f68718ad7a79970cc
Yağız Emre ÖZBİLGE <149567624+yagizemreozbilge@users.noreply.github.com>
2024-02-19 16:52:07 +0300

    Add files via upload

---
+++ Sample-Sorts/InsertionSORT.cpp
@@ -0,0 +1,64 @@
+﻿// InsertionSORT.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+void insertionSort(int* A, int n);
+
+int main() {
+    int A[6] = { 5,2,4,6,1,3 };
+    int n = sizeof(A) / sizeof(int);
+
+    insertionSort(A, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
+       
+    }
+
+    
+
+    
+ }
+ 
+
+
+        void insertionSort(int* A, int n) {
+        int key = 0;
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            key = A[j];
+            i = j - 1;
+            while ((i >= 0) && (A[i] > key)) {
+                A[i + 1] = A[i];
+                i = i - 1;
+                A[i + 1] = key;
+            }
+        }
+
+
+    
+
+
+    
+    
+    
+    }
+
+
+    
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Sample-Sorts/MergeSORT.cpp
@@ -0,0 +1,107 @@
+﻿
+
+    
+
+
+
+
+
+
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+
+
+
+void merge(int* A, int p, int q, int r) {
+
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    int* L = (int*)malloc(n1 * sizeof(int));
+    int* R = (int*)malloc(n2 * sizeof(int));
+
+    if (L == NULL || R == NULL) {
+        printf("Error");
+        exit(EXIT_FAILURE);
+        
+    }
+
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = A[p+i];
+    }
+    for (int j = 0; j < n2; j++) {
+        R[j] = A[q + j + 1];
+    }
+
+
+    int i = 0, j = 0, k = p;
+
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            A[k] = L[i];
+            i++;
+        }
+        else {
+            A[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        A[k] = L[i];
+        ++i;
+        ++k;
+    }
+
+    while (j < n2) {
+        A[k] = R[j];
+        ++j;
+        ++k;
+    }
+    free(L);
+    free(R);
+}
+
+
+
+void mergeSort(int* A, int l, int r) {
+    if (l < r) {
+        int m =  (r+l) / 2;
+        mergeSort(A, l, m);
+        mergeSort(A, m + 1, r);
+
+        merge(A, l, m, r);
+    }
+  }
+    
+    
+       
+    
+    
+
+
+    void printArray(int* A, int size) {
+        for (int i = 0; i < size; i++) {
+            printf("%d ", A[i]);
+        }
+
+    }
+
+
+    int main() {
+        int A[6] = { 5,2,4,6,1,3 };
+        int size = sizeof(A) / sizeof(A[0]);
+        mergeSort(A, 0, size - 1);
+        
+
+        printArray(A, size);
+        return 0;
+
+
+    }
+
